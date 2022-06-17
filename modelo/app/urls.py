3683d1846ce65553b5ca3a5eb8f970352bca1681
Sergio <se.placencia@duocuc.cl>
2022-06-17 17:41:35 -0400

    mejora de carrito e implementación de seguimiento

--- modelo/app/urls.py
+++ modelo/app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from app.views import eliminarsoporte,listadosoporte,soporte,modificarpedidos,listapedidosadm,datoseg,seguimiento,comprar,eliminarproductocarrito,cambiarpassword,registro,perfilusuario,crearperfil,index,perro,roedor,gatos,aves,anfibios,quienes,listado,crear,modificar,eliminar,perfil,listaproductos,modificarproductos,eliminarproductos,crearproductos,cart_add,cantidad,carrito
+from app.views import preparacion,encamino,entregado,eliminarsoporte,listadosoporte,soporte,modificarpedidos,listapedidosadm,datoseg,seguimiento,comprar,eliminarproductocarrito,cambiarpassword,registro,perfilusuario,crearperfil,index,perro,roedor,gatos,aves,anfibios,quienes,listado,crear,modificar,eliminar,perfil,listaproductos,modificarproductos,eliminarproductos,crearproductos,cart_add,cantidad,carrito
 
 urlpatterns = [
     path('', index, name='index'),
@@ -17,7 +17,7 @@ urlpatterns = [
     path('cambiarpassword/',cambiarpassword,name="cambiarpassword"),
     path('registro/',registro,name="registro"),
     path('perfilusuario/',perfilusuario,name="perfilusuario"),
-    path('perfil',perfil,name="perfil"),
+    path('perfil/',perfil,name="perfil"),
     path('listaproductos/',listaproductos,name="listaproductos"),
     path('modificarproductos/<id>',modificarproductos,name="modificarproductos"),
     path('eliminarproductos/<id>',eliminarproductos,name="eliminarproductos"),
@@ -28,7 +28,7 @@ urlpatterns = [
     path('carrito',carrito,name='carrito'),
     path('eliminarproductocarrito/<id>',eliminarproductocarrito,name='eliminarproductocarrito'),
     path('comprar/<id>/',comprar,name='comprar'),
-    path('seguimiento/',seguimiento,name='seguimiento'),
+    path('seguimiento/<id>',seguimiento,name='seguimiento'),
     path('datoseg/<id>/',datoseg,name='datoseg'),
     path('listapedidosadm',listapedidosadm,name='listapedidosadm'),
     path('modificarpedidos/<id>/',modificarpedidos,name='modificarpedidos'),
@@ -36,6 +36,10 @@ urlpatterns = [
     path('listadosoporte', listadosoporte, name="listadosoporte"),
     path('eliminarsoporte/<id>',eliminarsoporte,name="eliminarsoporte"),
 
+    path('preparacion',preparacion,name="preparacion"),
+    path('encamino',encamino,name="encamino"),
+    path('entregado',entregado,name="entregado"),
+    
 
     path('crearperfil',crearperfil,name="crearperfil")
     
